b79e687dbafc030033339b957d7996f870f5ffa9
jiangyf <jiangyf@rimepevc.com>
2023-05-22 15:52:15 +0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import logo from './logo.svg';
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
